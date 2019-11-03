b79d22640afa1fafc27be8ee923320c66c599c81
wata-pom <w.kumagai1224@gmail.com>
2019-11-03 15:26:21 +0900

    ゲームユーザー名を表現するUserNameクラスを実装
    文字数に関するルールを集約
    screen_nameメソッドによって仕様変更も楽!
    詳細

---
+++ user_name.py
@@ -0,0 +1,20 @@
+class UserName:
+    def __init__(self, name):
+        if not(4 <= len(name) <= 20):
+            raise ValueError(f"${name}は文字数のルール違反やで!")
+        self.name = name
+
+    def screen_name(self):
+        return self.name.upper()
+
+
+#UserNameのクラスインスタンス化
+
+hibiki = UserName(name = 'hibiki')
+
+# print(hibiki)
+# print(type(hibiki))
+print(hibiki.screen_name())
+
+# sho = UserName('wataru')
+# print(sho.name)
